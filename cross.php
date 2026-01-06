6c78691bea8eb06a61385e01fd215b1d12a46378
Orley Galán <orleigalan@gmail.com>
2026-01-05 23:07:58 -0500

    cross.php mejoras

--- cross.php
+++ cross.php
@@ -1,5 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
