c2eb17b73046bffa0e25ae1da775f57925cd58e4
Orley Galán <orleigalan@gmail.com>
2026-01-05 22:29:07 -0500

    probando

--- public/index.php
+++ public/index.php
@@ -4,24 +4,25 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Content-Type: application/json");
 
+echo "PHP FUNCIONANDO OK";
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-$uri = rtrim($uri, '/');
+// $uri = rtrim($uri, '/');
 
-$routes = [
-    '/usuario' =>  __DIR__ . '../vista/usuario_api.php',
-];
+// $routes = [
+//     '/usuario' =>  __DIR__ . '../vista/usuario_api.php',
+// ];
 
-if (!isset($routes[$uri])) {
-    http_response_code(404);
-    echo json_encode([
-        "success" => false,
-        "error" => "Endpoint no encontrado"
-    ]);
-    exit;
-}
+// if (!isset($routes[$uri])) {
+//     http_response_code(404);
+//     echo json_encode([
+//         "success" => false,
+//         "error" => "Endpoint no encontrado"
+//     ]);
+//     exit;
+// }
 
-require $routes[$uri];
+// require $routes[$uri];
 
-?>
+// ?>
