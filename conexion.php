2981c003f2fd45f2ad98b2ce3e9d825e2169e778
Orley Galán <orleigalan@gmail.com>
2026-01-05 23:44:09 -0500

    conexion.php2

--- conexion.php
+++ conexion.php
@@ -1,18 +1,16 @@
-<?php
+<?php 
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'facebook';
 
-$host = $_ENV['MYSQLHOST'];
-$port = $_ENV['MYSQLPORT'];
-$user = $_ENV['MYSQLUSER'];
-$pass = $_ENV['MYSQLPASSWORD'];
-$db   = $_ENV['MYSQLDATABASE'];
+$conn = mysqli_connect($host, $user,$pass, $db, );
 
-$conexion = mysqli_connect($host, $user, $pass, $db, $port);
+// if($conn){
+//     echo 'Conectado exitosamente';
+//     exit();
+// }else{
+//     echo 'Error de conexion ,' . mysqli_connect_error();
+// }
 
-if (!$conexion) {
-    http_response_code(500);
-    echo json_encode([
-        "success" => false,
-        "error" => "Error de conexión a la base de datos"
-    ]);
-    exit;
-}
+?>
