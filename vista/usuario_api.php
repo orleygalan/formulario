cabd3b7b905c915774d1329cee8252ce5c798bfd
Orley Galán <orleigalan@gmail.com>
2026-01-05 21:09:58 -0500

    ordenamiento de las cros

--- vista/usuario_api.php
+++ vista/usuario_api.php
@@ -1,10 +1,9 @@
 <?php
-require '../controlador/usuario_controlador.php';
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 
+require '../controlador/usuario_controlador.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 $data = json_decode(file_get_contents("php://input"), true);
