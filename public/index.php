00976541b0c2a7c51bf9eb405184fae7ee218152
Orley Galán <orleigalan@gmail.com>
2026-01-05 20:53:27 -0500

    Subiendo carpeta public falttante

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$uri = rtrim($uri, '/');
+
+$routes = [
+    '/usuario' => '../vista/usuario_api.php',
+];
+
+if (!isset($routes[$uri])) {
+    http_response_code(404);
+    echo json_encode([
+        "success" => false,
+        "error" => "Endpoint no encontrado"
+    ]);
+    exit;
+}
+
+?>
